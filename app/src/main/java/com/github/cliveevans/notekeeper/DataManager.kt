a8ab44593194f51c1cbdfe96e240c07e409e9e6e
Clive Evans <cliveevans@ntlworld.com>
2019-12-22 13:43:35 +0000

    Adds (some really bad) tests

--- app/src/main/java/com/github/cliveevans/notekeeper/DataManager.kt
+++ app/src/main/java/com/github/cliveevans/notekeeper/DataManager.kt
@@ -9,12 +9,29 @@ object DataManager {
         initialiseNotes()
     }
 
+    fun addNote(course: CourseInfo, notTitle: String, noteText: String): Int {
+        notes.add(NoteInfo(course, notTitle, noteText))
+        return notes.lastIndex
+    }
+
+    fun findNote(course: CourseInfo, noteTitle: String, noteText: String): NoteInfo? {
+        for (note in notes)
+            if (
+                course == note.course
+                &&
+                noteTitle == note.title
+                &&
+                noteText == note.text
+            ) return note
+        return null
+    }
 
     private fun initializeCourses() {
         var course = CourseInfo("android_intents", "Android Programming with Intents")
         courses.set(course.courseId, course)
 
-        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        course =
+            CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
         courses.set(course.courseId, course)
 
         course = CourseInfo(courseId = "java_lang", title = "Java Fundamentals: The Java Language")
@@ -24,7 +41,7 @@ object DataManager {
         courses.set(course.courseId, course)
     }
 
-    private fun initialiseNotes() {
+    fun initialiseNotes() {
         courses.values.forEach { c: CourseInfo ->
             notes.add(NoteInfo(c, "title for" + c.title, "A note about it"))
         }
